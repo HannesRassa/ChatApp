b58bdaa239011bf7dd44ab7b65c262dc3d885ece
Eduard Andrianov <edandr@taltech.ee>
2024-10-09 16:11:19 +0300

    Added "PlayerController"

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,32 @@
+using BackEnd.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd.Controllers
+{
+    [Route("Backend/Player")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public PlayerController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var players = _context.Players.ToList();
+            return Ok(players);
+        }
+
+        [HttpGet("id")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var player = _context.Players.Find(id);
+
+            if (player == null) return NotFound();
+
+            return Ok(player);
+        }
+    }
+}
--- obj/Debug/net8.0/backEnd.AssemblyInfo.cs
+++ obj/Debug/net8.0/backEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c9d7ac54c313aee03e15a958fdf4d545a8a6015")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd4aabe690e567469ba10a617c7dafa46e103f9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("backEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
