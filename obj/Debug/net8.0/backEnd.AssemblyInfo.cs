03d25be82a5ff36ff23f6a3a64ffa4f335a424b1
Daniil Rakitin <daraki@taltech.ee>
2024-10-24 18:55:58 +0300

    LobbyPage_added_index.vue

--- obj/Debug/net8.0/backEnd.AssemblyInfo.cs
+++ obj/Debug/net8.0/backEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2f2a5e377b2567183a541511d11b32c2902e790")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f316f23d93ae6ba85ca93d0f7b939a3f19c8d3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("backEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
